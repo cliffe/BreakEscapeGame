a65ba12d1b81241a4cc666920d2b29a4c64a3eb4
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2026-07-20 08:50:34 +0100

    chore: bump version to 1.0.7

--- lib/break_escape/version.rb
+++ lib/break_escape/version.rb
@@ -1,6 +1,6 @@
 module BreakEscape
   # Bump this when you make a new release.
   # Cache busting for assets -- updating this will force browsers to fetch new versions of all the JS, CSS, and most character assets.
-  VERSION = '1.0.6'
+  VERSION = '1.0.7'
   ASSETS_VERSION = ENV.fetch('BREAK_ESCAPE_ASSETS_VERSION', VERSION)
 end
